c8bf7b3451896df63afecfcb853521ed65732990
Ishaq <auwalishaq113@gmail.com>
2026-03-25 09:53:01 -0700

    commited

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
 const authController = require("../controllers/auth.controller");
 const middlewares = require("../middleware/auth.middleware.js");
-=======
-const authController = require("../controllers/auth.controller.js");
->>>>>>> 4feece19e3750df76da52afad14afe050dcc189a
 
 router.post("/auth/signup", authController.signup);
 router.post("/auth/login", authController.login);
